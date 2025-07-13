e3583fab7d4341da5d3430880489bfeac240d7a2
Konstantinos OIkonomopoulos <146142720+dipla8@users.noreply.github.com>
2025-07-13 21:52:32 +0300

    Delete fifo_video directory

--- fifo_video/temp/FIFO/fifo_define.v
+++
@@ -1,3 +0,0 @@
-`define module_name fifo_video
-`define getname(oriName,tmodule_name) \~oriName.tmodule_name 
-`define LUT_BASED
--- fifo_video/temp/FIFO/fifo_parameter.v
+++
@@ -1,6 +0,0 @@
-parameter WDEPTH = 2;
-parameter WDSIZE = 16;
-parameter RDEPTH = 2;
-parameter RDSIZE = 16;
-parameter ASIZE = 1;
-parameter RASIZE = 1;
